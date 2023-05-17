3998d66de8617837f2bd92329f1322aa034f7d64
Marcin <94755972+Marcinzale@users.noreply.github.com>
2023-05-17 20:20:09 +0200

    Initial Commit

---
+++ app/src/main/java/com/marcinzale/jatpackcomposetimer/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.marcinzale.jatpackcomposetimer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.marcinzale.jatpackcomposetimer.ui.theme.JatpackComposeTimerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun Timer(
+    totalTime: Long,
+    handleColor: Color,
+    inactiveBarColor: Color,
+    activeBarColor: Color,
+    modifier: Modifier = Modifier,
+    initialValue: Float = 0f,
+    strokeWidth: Dp = 5.dp
+) {
+
+}
